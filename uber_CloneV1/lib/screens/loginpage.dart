a683d764932791e74878ddffa821b9fb03e2fe73
mostafa <mostafa.morsy@msa.edu.eg>
2021-04-11 05:41:00 +0200

    To Be Continue

--- uber_CloneV1/lib/screens/loginpage.dart
+++ uber_CloneV1/lib/screens/loginpage.dart
@@ -1,3 +1,4 @@
+import 'package:cab_rider/brand_colors.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -5,33 +6,84 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          SizedBox(
-            height: 70,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 70,
+                ),
+                Image(
+                  image: AssetImage('images/logo.png'),
+                  alignment: Alignment.center,
+                  height: 100.0,
+                  width: 100.0,
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  "Sign In As a Rider",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25, fontFamily: 'Brand-Bold'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            labelText: 'Email Address',
+                            labelStyle: TextStyle(fontSize: 15.0),
+                            hintStyle:
+                                TextStyle(fontSize: 10, color: Colors.grey)),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(fontSize: 15.0),
+                            hintStyle:
+                                TextStyle(fontSize: 10, color: Colors.grey)),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      RaisedButton(
+                        onPressed: () {},
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        color: BrandColors.colorGreen,
+                        textColor: Colors.white,
+                        child: Container(
+                          height: 50,
+                          child: Center(
+                            child: Text(
+                              "LOGIN",
+                              style: TextStyle(
+                                  fontSize: 18, fontFamily: 'Brand-Bold'),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () {},
+                  child: Text("Don\'t have an account, sign up here"),
+                )
+              ],
+            ),
           ),
-          Image(
-            image: AssetImage('images/logo.png'),
-            alignment: Alignment.center,
-            height: 100.0,
-            width: 100.0,
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Text(
-            "Sign In As a Rider",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 25, fontFamily: 'Brand-Bold'),
-          ),
-          TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-                labelText: 'Email Address',
-                labelStyle: TextStyle(fontSize: 15.0),
-                hintStyle: TextStyle(fontSize: 10, color: Colors.grey)),
-          ),
-        ],
+        ),
       ),
     );
   }
