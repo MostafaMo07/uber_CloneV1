69bd7c221efbb8670a18079b914c65695d4735f1
mostafa <mostafa.morsy@msa.edu.eg>
2021-04-12 14:59:01 +0200

    Map Setting Page

---
+++ uber_CloneV1/lib/globalkey.dart
@@ -0,0 +1,1 @@
+String mapKey = "AIzaSyB8ID-ySXJJWd5mQ02X807BAijhPcm7l4s";
--- uber_CloneV1/lib/screens/mainpage.dart
+++ uber_CloneV1/lib/screens/mainpage.dart
@@ -1,5 +1,10 @@
+import 'package:cab_rider/brand_colors.dart';
+import 'package:cab_rider/widgets/BrandDivider.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
+import 'package:outline_material_icons/outline_material_icons.dart';
 
 class MainPage extends StatefulWidget {
   static const String id = 'mainpage';
@@ -9,24 +14,159 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  Completer<GoogleMapController> _controller = Completer();
+  GoogleMapController mapController;
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Main Page"),
-      ),
-      body: Center(
-        child: MaterialButton(
-          onPressed: () {
-            DatabaseReference dbref =
-                FirebaseDatabase.instance.reference().child("Test");
-            dbref.set("Is Connected");
-          },
-          height: 50,
-          minWidth: 300,
-          color: Colors.green,
-          child: Text("Test Connection"),
-        ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            myLocationButtonEnabled: true,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+              mapController = controller;
+            },
+          ),
+          Positioned(
+            right: 0,
+            left: 0,
+            bottom: 0,
+            child: Container(
+              height: 300,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 15.0,
+                        spreadRadius: 0.5,
+                        offset: Offset(0.7, 0.7))
+                  ]),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "Nice to see your",
+                      style: TextStyle(fontSize: 10),
+                    ),
+                    Text(
+                      "Where are you going?",
+                      style: TextStyle(fontSize: 18, fontFamily: "Brand-Bold"),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(4),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black12,
+                                blurRadius: 5.0,
+                                spreadRadius: 0.5,
+                                offset: Offset(0.7, 0.7))
+                          ]),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.search,
+                              color: Colors.blueAccent,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text("Search Destination"),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 22,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          OMIcons.home,
+                          color: BrandColors.colorDimText,
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Add Home"),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              "Your Resdential Address",
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  color: BrandColors.colorDimText),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    BrandDivider(),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          OMIcons.workOutline,
+                          color: BrandColors.colorDimText,
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Add Work"),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              "Your Officer Address",
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  color: BrandColors.colorDimText),
+                            )
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
